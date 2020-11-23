875b84bef0324338c6620630a71078a14aa19ee9
gwynnemeeks <63276406+gwynnemeeks@users.noreply.github.com>
2020-11-23 13:22:39 -0600

    Delete brazil_customers_invoices.sql

--- brazil_customers_invoices.sql
+++
@@ -1,7 +0,0 @@
-SELECT i.InvoiceId,
-    i.InvoiceDate,
-    c.FirstName || '' || c.LastName as FullName,
-    i.BillingCountry
-FROM Invoice i
-    join Customer c on i.CustomerId = c.CustomerId
-WHERE c.Country = 'Brazil';
